dd6e6593ae60cb47a9bcc67749b091a6e1f7b4a7
Tony <kritikostony@gmail.com>
2021-10-13 10:10:50 +0300

    Travis setup

--- app/dashboard.py
+++ app/dashboard.py
@@ -10,8 +10,8 @@ import datetime
 
 # connection = mysql.connector.connect(host=credential.host, database=credential.database, user=credential.user,
 #                                      password=credential.password)
-connection = mysql.connector.connect(host=$HOST, database=$DATABASE, user=$USER,
-                                     password=$PASSWORD)
+connection = mysql.connector.connect(host="$HOST", database="$DATABASE", user="$USER",
+                                     password="$PASSWORD")
 
 
 def sql_query(command):
