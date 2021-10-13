95415e8835400a5a5471c33ee373079f751b4090
Tony <kritikostony@gmail.com>
2021-10-13 11:36:13 +0300

    Travis setup

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,3 +1,5 @@
+import os
+
 import dash
 from dash import dcc
 from dash import html
@@ -10,6 +12,10 @@ import datetime
 #
 # connection = mysql.connector.connect(host=credential.host, database=credential.database, user=credential.user,
 #                                      password=credential.password)
+host = os.getenv($HOST)
+database = os.getenv($DATABASE)
+user = os.getenv($USER)
+password = os.getenv($PASSWORD)
 connection = mysql.connector.connect(host=host, database=database, user=user,
                                      password=password)
 
