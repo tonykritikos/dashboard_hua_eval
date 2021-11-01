e8dbe2a965c6af4eaed62c56f92119119597ea61
Tony <kritikostony@gmail.com>
2021-11-01 22:52:49 +0200

    Rearranged the code to be more structured Commented Renamed values with more on-point names

--- app/config.py
+++ app/config.py
@@ -14,18 +14,22 @@ median_columns = ['q3_cleargoalslectures', 'q4_cleargoalslabs', 'q5_studymateria
                   'q10_tutorontime', 'q11b_assistanthelps',
                   'q12_materialcovered',
                   'q13_tutororganised', 'q14_evaluationcriteria']
+dict_median_columns = [("q3", "q3_cleargoalslectures"),
+                       ("q4", "q4_cleargoalslabs"),
+                       ("q5", "q5_studymaterial"),
+                       ("q6", "q6_tutorinteresting"),
+                       ("q7", "q7_tutorquestions"),
+                       ("q8", "q8_tutorreachable"),
+                       ("q9", "q9_tutorexplains"),
+                       ("q10", "q10_tutorontime"),
+                       ("q11", "q11b_assistanthelps"),
+                       ("q12", "q12_materialcovered"),
+                       ("q13", "q13_tutororganised"),
+                       ("q14", "q14_evaluationcriteria")]
 median_columns_plus_year = ['q3_cleargoalslectures', 'q4_cleargoalslabs', 'q5_studymaterial',
-                     'q6_tutorinteresting',
-                     'q7_tutorquestions', 'q8_tutorreachable', 'q9_tutorexplains',
-                     'q10_tutorontime', 'q11b_assistanthelps',
-                     'q12_materialcovered',
-                     'q13_tutororganised', 'q14_evaluationcriteria', 'qyear']
-median_columns_plus_courseid = ['q3_cleargoalslectures', 'q4_cleargoalslabs', 'q5_studymaterial',
-                       'q6_tutorinteresting',
-                       'q7_tutorquestions', 'q8_tutorreachable', 'q9_tutorexplains',
-                       'q10_tutorontime', 'q11b_assistanthelps',
-                       'q12_materialcovered',
-                       'q13_tutororganised', 'q14_evaluationcriteria', 'courseid']
-
-
+                            'q6_tutorinteresting',
+                            'q7_tutorquestions', 'q8_tutorreachable', 'q9_tutorexplains',
+                            'q10_tutorontime', 'q11b_assistanthelps',
+                            'q12_materialcovered',
+                            'q13_tutororganised', 'q14_evaluationcriteria', 'qyear']
 year = int((datetime.datetime.now().strftime('%Y')))
